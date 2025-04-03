639120a04e4b42a9131c2c5c388f73877d455dc5
swizzcodes <swizzcodes929@gmail.com>
2025-04-03 20:16:48 +0530

    DA2.py

--- DA2.py
+++ DA2.py
@@ -1,1 +1,33 @@
+#Printing Strings using Single quotes ('')
+
+print('Hello world')
+
+#Printing Strings using Double Quotes ("")
+
+print("Hello world")
+
+#Performing Mathematical Operations using Python double quotes as this may print the query to the screen
+print(500+500) #Output 1000
+print(500-500) #Output 0
+print(500*500) #Output 250000
+print(500/500) #Output 1.0
+
+#Variables and types of datatypes in python
+
+var1 = "Variable 1" #This variable holds a string 
+var2 = 100          #This variable holds an integer
+var3 = 105.5        #This variable holds a float value
+var4 = True         #This variable holds a boolean
+var5 = False        #This variable also holds a boolean
+var6 = ["one","two","three"]     #This variable holds a list
+var7 = {
+  key:value         #This va
+#Note that while performing Mathematical Operations you should not use single orriable holds a dictionary
+}
+var8 = (10,20)      #This variable holds a tuple
+
+#Printing multiple variables using print()
+
+print(var1, var2, var3, var4, var5) #Output Variable 1 100 105.5 True False
+
 
